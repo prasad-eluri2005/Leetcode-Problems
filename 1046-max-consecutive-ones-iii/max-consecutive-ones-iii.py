7b6ae1cb60cfc93f7c7c1c621b98e5da6cb0a24b
prasad-eluri2005 <24b11ds056@adityauniversity.in>
2026-08-16 22:21:21 +0530

    Time: 66 ms (26.48%) | Memory: 22.1 MB (50.15%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -4,19 +4,13 @@ class Solution:
         j = 0
         cnt = 0
         m = 0
-
         while j < len(nums):
-
             if nums[j] == 0:
                 cnt += 1
-
             j += 1
-
             while cnt > k:
                 if nums[i] == 0:
                     cnt -= 1
                 i += 1
-
             m = max(m, j - i)
-
         return m
